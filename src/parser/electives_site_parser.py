d854224d9566848a221f06d208273b017cc74c87
YoiOni <Daratars@gmail.com>
2022-05-24 01:19:46 +0500

    Add correct append data

--- src/parser/electives_site_parser.py
+++ src/parser/electives_site_parser.py
@@ -1,17 +1,25 @@
+import re
 from logging import getLogger
+
+from bs4 import BeautifulSoup
 from sqlalchemy.orm import sessionmaker
-from models_db import *
-from parser.result_model import ResultModel
+from src.models_db import *
+from src.parser.result_model import ResultModel
 from typing import List
 from .abstract_site_parser import AbstractSiteParser
 import datetime
+from requests import get
 
 
 logger = getLogger(__name__)
 
 
 class ElectivesSiteSiteParser(AbstractSiteParser):
-    _HEADERS = 'headers'
+    _HEADERS = {
+        "user-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/99.0.4844.84 Safari/537.36",
+        "accept": "*/*"
+    }
 
     def __init__(self, conn_db, params):
         super(ElectivesSiteSiteParser, self).__init__(params)
@@ -29,10 +37,58 @@ class ElectivesSiteSiteParser(AbstractSiteParser):
     def __call__(self):
         self._send_data_to_db()
 
-    def _handle_data(self) -> List[ResultModel]:
-        data = self._send_request()
-        data: List[ResultModel]
-        return data
+    def _handle_data(self):
+        data = self._send_request().text
+        soup = BeautifulSoup(data, "lxml")
+        minor_list = soup.find_all("li")
+        result_list = []
+        for minor in minor_list:
+            elective_list = minor.find_all("div", class_="b-minor-elective--item")
+            for elective in elective_list:
+                elective_attributes = []
+                elective_title = elective.find("div", class_="b-minor-elective--title");
+                elective_attributes.append(elective_title.text.strip())
+                elective_attributes.append(re.sub("^\s+|\n|\r|\s+$", '', elective.find("div",
+                                                                                       class_="b-minor-elective--descr").text))
+                elective_url = 'https://www.utmn.ru' + elective_title.find("a").get("href")
+                elective_page = get(elective_url, headers=self._HEADERS).text
+                page_soup = BeautifulSoup(elective_page, 'lxml')
+                full_descr = page_soup.find("div", class_='b-minor-content_text')
+                if full_descr is not None:
+                    elective_attributes.append(full_descr.text.strip())
+                else: elective_attributes.append('None')
+
+                authors_list = elective.find_all("div", class_="b-minor-elective--author")
+                author_names = []
+                author_descrs =[]
+                for author in authors_list:
+                    author_name = author.find("a", class_="b-minor-elective--author-name")
+                    author_descr = author.find("div", class_="b-minor-elective--author-descr")
+                    if author_name is not None:
+                        author_names.append(
+                            re.sub("^\s+|\n|\r|\s+$", '', author_name.text))
+                    else: author_names.append('None')
+                    if author_descr is not None:
+                        author_descrs.append(re.sub("^\s+|\n|\r|\s+$", '', author_descr.text))
+                    else: author_descrs.append('None')
+
+                elective_attributes.append(author_names)
+                elective_attributes.append(author_descrs)
+                tags = []
+                tag_list = elective.find_all("div", class_="b-minor-elective--directions__item")
+                for tag in tag_list: tags.append(tag.text.strip())
+                elective_attributes.append(tags)
+                elective_attributes.append(minor.find("div", class_="b-minor-elective--title").text.strip())
+                result_list.append({
+                    'title': elective_attributes[0],
+                    'short_description': elective_attributes[1],
+                    'full_description': elective_attributes[2],
+                    'elective_authors': elective_attributes[3],
+                    'author_description': elective_attributes[4],
+                    'elective_tags': elective_attributes[5],
+                    'minor': elective_attributes[6]
+                })
+        return result_list
 
     def _commit_db_objects(
         self,
@@ -99,7 +155,7 @@ class ElectivesSiteSiteParser(AbstractSiteParser):
         session.commit()
 
     def _send_data_to_db(self):
-        data_stub = []
+        data_stub = self._handle_data()
         logger.info("STARTED SENDING DATA...")
         begin_time = datetime.datetime.now()
         for item in data_stub:
